86993f1603e838d3992f2b3897374b5c635332ef
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-05-07 09:30:27 +0700

    Pembuatan kerangka awal

---
+++ src/views/loginforms/LoginFormView.vue
@@ -0,0 +1,3 @@
+<template src="./loginformview.html"></template>
+<script src="./loginformview.js"></script>
+<style src="./loginformview.css"></style>
---
+++ src/views/loginforms/loginformview.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'loginform',
+  data() {
+    return {
+      value: '',
+    };
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+};
